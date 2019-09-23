e48c19948833793070e36b7f1946aa1ac50da8f3
jeffyjkang <jeffyjkang@gmail.com>
2019-09-23 14:08:58 -0700

    track display working

--- athlete/src/com/lambdaschool/solution/Main.java
+++ athlete/src/com/lambdaschool/solution/Main.java
@@ -4,29 +4,38 @@ public class Main
 {
     public static void main(String[] args)
     {
+        String athlete1 = "Runner";
+        String athlete2 = "Goalie";
+        String athlete3 = "Athlete";
+
+        String sport1 = "Track";
+        String sport2 = "Hockey";
+        String sport3 = "Rugby";
+        String sport4 = "Baseball";
+
         AthleteCreationInjector injector;
         Processor app;
 
         // Create a Track Athlete
         injector = new TrackAthleteCreationInjector();
         app = injector.getProcess();
-        app.displayAthlete();
+        app.displayAthlete(sport1, athlete1);
 
-        // Create a Hockey Athlete
-        injector = new HockeyAthleteCreationInjector();
-        app = injector.getProcess();
-        app.displayAthlete();
-
-
-        // Create a Rugby Athlete
-        injector = new RugbyAthleteCreationInjector();
-        app = injector.getProcess();
-        app.displayAthlete();
-
-        // Create a Baseball Athlete
-        injector = new BaseballAthleteCreationInjector();
-        app = injector.getProcess();
-        app.displayAthlete();
+//        // Create a Hockey Athlete
+//        injector = new HockeyAthleteCreationInjector();
+//        app = injector.getProcess();
+//        app.displayAthlete(sport2, athlete2);
+//
+//
+//        // Create a Rugby Athlete
+//        injector = new RugbyAthleteCreationInjector();
+//        app = injector.getProcess();
+//        app.displayAthlete(sport3, athlete3);
+//
+//        // Create a Baseball Athlete
+//        injector = new BaseballAthleteCreationInjector();
+//        app = injector.getProcess();
+//        app.displayAthlete(sport4, athlete3);
 
     }
 }
